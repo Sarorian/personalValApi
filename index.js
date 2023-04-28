89d3b6a94f86c7d0c6d21ac30a4b80b883878816
Sarorian <shamowen5@gmail.com>
2023-04-28 03:26:33 -0600

    Doing things

---
+++ index.js
@@ -0,0 +1,92 @@
+require('dotenv').config();
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const mongoose = require('mongoose'),
+        Models = require('./models.js'),
+        teamData = Models.teamData;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send("Trying to get team data for BYDO?")
+})
+
+app.get('/teamdata', (req, res) => {
+    teamData.find()
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send('Error: ' + err)
+        })
+})
+
+app.get('/teamdata/:map', (req, res) => {
+    teamData.find({ map: req.params.map})
+    .then((data) => {
+        res.status(200).json(data);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).send('Error: ' + err)
+    })
+})
+
+app.get('/players/:playerName', (req, res) => {
+    const playerName = req.params.playerName;
+    const playerModel = Models[playerName + 'Data'];
+  
+    if (!playerModel) {
+        res.status(400).send('Invalid player name');
+        return;
+    }
+  
+    playerModel.find()
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send('Error: ' + err);
+        });
+  });
+
+app.get('/players/:playerName/:agent', (req, res) => {
+    const playerName = req.params.playerName;
+    const playerModel = Models[playerName + 'Data'];
+  
+    if (!playerModel) {
+        res.status(400).send('Invalid player name');
+        return;
+    }
+  
+    playerModel.find({ agent: req.params.agent })
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send('Error: ' + err);
+        });
+  });
+  
+const connectDB = async () => {
+    try {
+        mongoose.connect(`mongodb+srv://shamowen5:${process.env.ATLAS_PASS}@sarorian.wh5lzfz.mongodb.net/BYDO`, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Connected to database sucesfully");
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+connectDB();
+
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
+});
