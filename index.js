1938f9777aebb2c0a0d4ccac6c3473a2b5153d6e
Sarorian <shamowen5@gmail.com>
2023-05-04 14:55:37 -0600

    fixed cors

--- index.js
+++ index.js
@@ -10,7 +10,18 @@ const mongoose = require('mongoose'),
         teamData = Models.teamData;
 
 app.use(bodyParser.json());
-
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:1234']
+app.use(cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1){
+        let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+        return callback(new Error(message ), false);
+      }
+      return callback(null, true);
+    }
+  }));
 app.get('/', (req, res) => {
     res.send("Trying to get team data for BYDO?")
 })
